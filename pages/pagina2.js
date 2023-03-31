450bc8a9320e106760145c09769dd68efa5f525c
Ismael Correia <rogerio.ismael@gmail.com>
2023-03-30 21:57:07 -0300

    Aula 30/03

---
+++ pages/pagina2.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Pagina from '../components/Pagina';
+
+const pagina2 = () => {
+  return (
+    <>
+      <Pagina />
+      <Container>
+        
+    <p>Qualquer Texto</p>
+    <p>Qualquer Texto</p>
+      
+
+      </Container>
+      
+    </>
+  )
+}
+
+export default pagina2
